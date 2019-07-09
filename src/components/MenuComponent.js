a3434a9597e0f4669f73446884d7963fa80c16d4
Karthigeyan420 <karthi42096@gmail.com>
2019-07-09 11:42:44 +0530

    Presentational and Container component- commit2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,35 +1,7 @@
 import React, {Component} from 'react';
-import DishDetail from './DishDetailComponent'
-import Main from './MainComponent'
-import {Card, CardImg, CardImgOverlay, CardText, CardTitle, CardBody} from 'reactstrap'
+import {Card, CardImg, CardImgOverlay,  CardTitle} from 'reactstrap';
 
 class Menu extends Component {
-    constructor(props){
-        super(props);
-        
-        this.state = { selectedDish : null }
-
-    }
-    onDishSelect(dish){
-        this.setState({ selectedDish: dish });
-    }
-
-    renderDish(dish){
-        if (dish != null){
-            return (
-                <Card>
-                    <CardImg width="100%" object src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }
-        else{
-            return(<div></div>)
-        }
-    }
 
     render(){
         
